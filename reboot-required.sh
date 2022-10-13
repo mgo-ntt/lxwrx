81ad2d1c49279150b4ca6aa022e0fbef9107d04b
Michael Goessmann Matos <dj0nz@users.noreply.github.com>
2022-10-13 10:05:02 +0200

    CheckMK local check
    Raise warning if an installed package or upgrade demands a reboot.

--- reboot-required.sh
+++ reboot-required.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# CheckMK local check for Debian
+# Raise warning if installed update demands a reboot
+# 
+# dj0Nz Dec 2021
+
 if [ -f /var/run/reboot-required ]; then
    REASON=`cat /var/run/reboot-required.pkgs`
    echo "1 \"Pending reboot\" - Reboot requested by package $REASON"
