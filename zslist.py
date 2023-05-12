7b76021c267dbdba06c88c6b0bcc9252ed08eb82
Michael Goessmann Matos <dj0nz@users.noreply.github.com>
2023-05-12 13:17:21 +0200

    Create ipset with Zscaler hubs

--- zslist.py
+++ zslist.py
@@ -1,9 +1,10 @@
-# list zscaler ipv4 hubs 
+# create ipset with zscaler ipv4 hubs to use in iptables firewall ruleset
 # dj0Nz apr 2023
 
 import json
 import ipaddress
 import requests
+import os
 
 # function to check if input is a valid ipv4 network
 def is_ipv4_net(network):
@@ -20,7 +21,13 @@ zs_hublist = response.json()
 # extract hub prefixes list
 hub_prefixes = zs_hublist['hubPrefixes']
 
-# loop through prefixes list and print entries
+# create or reset zsclaer ipset
+os.system('/usr/sbin/ipset -q create zscaler hash:net')
+os.system('/usr/sbin/ipset flush zscaler')
+
+# loop through prefixes list and create ipset entries
 for hub in hub_prefixes:
     if is_ipv4_net(hub):
-        print(hub)
+        add_net = "/usr/sbin/ipset add zscaler " + str(hub) 
+        os.system(add_net)
+
